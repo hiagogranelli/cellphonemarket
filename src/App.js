392de5cbed349bf763e61c5140cbac8a4fb2231a
unknown <hiagogr23@gmail.com>
2019-11-06 23:04:31 -0300

    Navegação com History

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify'
 
@@ -9,17 +9,18 @@ import GlobalStyle from './Styles/Global';
 import Header from './Components/Header';
 import Routes from './routes';
 
+import history from './Services/history'
 import store from './Store'
 
 export default function src() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      <Router history={history}> 
         <Header />
         <Routes />
         <GlobalStyle />
         <ToastContainer autoClose={3000} />
-      </BrowserRouter>
+      </Router>
     </Provider>
   );
 }

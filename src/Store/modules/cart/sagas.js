2562c2b0ec96a9d6a149c120cbb6f01de6be751a
unknown <hiagogr23@gmail.com>
2019-11-06 22:14:08 -0300

    Redux Saga + Reactotron Saga Config

---
+++ src/Store/modules/cart/sagas.js
@@ -0,0 +1,13 @@
+import { call, put, all, takeLatest } from 'redux-saga/effects';
+
+import api from '../../../Services/api'
+
+import { addToCartSuccess } from './actions'
+
+function* addToCart( { id }) {
+    const response = yield call(api.get, `/products/${id}`);
+
+    yield put(addToCartSuccess(response.data));    
+}
+
+export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
